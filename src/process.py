3dc56f3341ac750ddb5338bb04a3fa2b82295e5f
jmsiro <jmsiro@gmail.com>
2022-06-07 23:22:15 -0300

    Sin terminar excel

--- src/process.py
+++ src/process.py
@@ -1,48 +1,61 @@
+from dataclasses import replace
 import pandas as pd
 import engine
 import logging
 from datetime import datetime
+import os
+from openpyxl import Workbook
 
 def reg_q():
     
     log_name = f"process_{datetime.now().strftime('%Y-%m-%d')}.log"
-
     logging.basicConfig(filename=log_name, level = logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S')
     
     en = engine.eng_con()
     
     results = en.execute("""
-                SELECT "Categoría", COUNT("Cod_Loc")
+                SELECT "Categoría", COUNT("Cod_Loc") AS "Cantidad por categoría"
                 FROM alkemydb
                 GROUP BY "Categoría";
                """)
     
-    df = pd.DataFrame(results)    
-    logging.info(df)
+    df = pd.DataFrame(results)
     
     results1 = en.execute("""
-                SELECT COUNT(*)
+                SELECT
+                    'Museos' as "Fuente",
+                    COUNT(*) as "Cantidad de registros"
                 FROM museos
                 UNION
-                SELECT COUNT(*)
+                SELECT
+                    'Bibliotecas' as "Fuente",
+                    COUNT(*) as "Cantidad de registros"
                 FROM bibliotecas
                 UNION
-                SELECT COUNT(*)
+                SELECT
+                    'Salas de Cine' as "Fuente",
+                    COUNT(*) as "Cantidad de registros"
                 FROM salas_cine;
                """)
 
     df1 = pd.DataFrame(results1)    
-    logging.info(df1)
 
     results2 = en.execute("""
-                SELECT "Categoría", "Provincia", COUNT("Cod_Loc")
+                SELECT "Categoría", "Provincia", COUNT("Cod_Loc") AS "Cantidad por Provincia"
                 FROM alkemydb
                 GROUP BY "Categoría", "Provincia"
                 ORDER BY "Categoría", "Provincia";
                """)
     
     df2 = pd.DataFrame(results2)    
-    logging.info(df2)
 
+    wb = Workbook()
+    name = "proccess_results" + "/" + datetime.now().strftime("%d-%m-%Y")
+    wb.save(filename=name)
+
+    with pd.ExcelWriter(name + ".xlsx", if_sheet_exists=replace) as writer:
+        df.to_excel(writer, sheet_name="Registros totales por categoría")
+        df1.to_excel(writer, sheet_name="Registros totales por fuente")
+        df2.to_excel(writer, sheet_name="Registros por provincia y categoría")
 reg_q()
 
